8a08383635e4f0062e8c4fcc4afb5bb7c4ba96f1
gkalyan04 <g.kalyan04@gmail.com>
2017-10-19 17:16:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/calculator/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.android.calculator;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.R.attr.button;
+import static java.lang.Integer.parseInt;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+
+            }
+
+
+        }, SPLASH_TIME_OUT);
+    }
+}
